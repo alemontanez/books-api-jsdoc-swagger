a8409a17c49f77537f3007c7389d68a22a309833
alemontanez <alemontanez99@gmail.com>
2025-07-11 18:12:20 -0300

    Configuración de base de datos separada para entorno de testing

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,18 +1,11 @@
 import { config } from 'dotenv'
-
 config()
 
-// function requireEnv(varName: string): string {
-//   const value = process.env[varName]
-//   if (!value) {
-//     throw new Error(`La variable de entorno ${varName} es obligatoria`)
-//   }
-//   return value
-// }
+export const NODE_ENV = process.env.NODE_ENV || 'development'
 
 export const PORT = process.env.PORT
 export const PG_HOST = process.env.PG_HOST
 export const PG_PORT = process.env.PG_PORT
 export const PG_USER = process.env.PG_USER
 export const PG_PASSWORD = process.env.PG_PASSWORD
-export const PG_DATABASE = process.env.PG_DATABASE
+export const PG_DATABASE = NODE_ENV === 'test' ? process.env.PG_DATABASE_TEST : process.env.PG_DATABASE
